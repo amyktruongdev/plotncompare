ed6dcaa302df1858b52e0dd01d4120f3d8cd0162
Amy <amyktruongdev@gmail.com>
2025-03-25 11:58:46 -0700

    matplotlib error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 st.set_page_config(page_title="Plot and Compare", layout="centered")
 st.title("Plot and compare xlsx")
